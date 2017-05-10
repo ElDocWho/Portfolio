a6d52c194840c6373310d0fb1b6e2baf65ff2596
Fortunato <fortunato.maycotte@gmail.com>
2017-05-10 13:53:31 -0700

    Created 3 objects

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function JobHistory(companyName, namePosition, namePosition){
+  this.companyName = companyName;
+  this.positionName = namePosition;
+  this.cityLocation = cityLocation;
+};
+
+function PortfolioProjects (projectName, projectUrl, projectDescription ){
+  this.projectName = projectName;
+  this.projectURL = projectUrl;
+  this.projectDescription = projectDescription;
+}
+function EducationHistory (schoolName,cityName, studyField) {
+  this.schoolName = schoolName;
+  this.cityName = cityName;
+  this.studyFIeld = studyField;
+}
