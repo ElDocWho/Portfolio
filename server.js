ed1ed208934e802b8702269220b73d9ccdb1bb32
Fortunato <fortunato.maycotte@gmail.com>
2017-05-31 04:07:30 -0700

    server up and running

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, function() {
+  console.log('We are serving this app on localhost:' + PORT);
+});
